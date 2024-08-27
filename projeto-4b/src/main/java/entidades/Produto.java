50b584dd3e9d07c3348253432b0a20a8843edab6
mauro-roque <128740268+mauro-roque@users.noreply.github.com>
2024-08-26 22:36:14 -0300

    Add files via upload

---
+++ projeto-4b/src/main/java/entidades/Produto.java
@@ -0,0 +1,12 @@
+package entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Produto {
+    public Integer codigo;
+    public String nome;
+    public Short quantidade;
+    public BigDecimal preco;
+    public LocalDate validade;
+}
